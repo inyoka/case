650db8a181b6ee75c5c2927b3bbe30eac3e6a210
Simon <simonmarkholland@gmail.com>
2018-10-01 13:15:38 +0700

    Delete settings_secret.py

--- case/settings/settings_secret.py
+++
@@ -1,7 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hzx)xh9+*j@h0hw0o3g2&pwf%f&wjx6#cv9f+8*%meeuqvhpf+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1','casemedan.pythonanywhere.com','www.casemedan.com']
